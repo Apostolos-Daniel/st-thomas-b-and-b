79fbed27a944b496ed3d038c227e141103d11f53
Toli <toli@flipdish.com>
2024-02-13 09:08:21 +0000

    try 'hello-world' function that returns html

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,4 +1,4 @@
-import { Api, Config, StackContext, use } from "sst/constructs";
+import { Api, Function, StackContext, use } from "sst/constructs";
 import { StorageStack } from "./StorageStack";
 //import { Datadog } from "datadog-cdk-constructs-v2";
 
@@ -6,19 +6,36 @@ export function ApiStack({ stack }: StackContext) {
   const { table } = use(StorageStack);
   // Create the API
   const api = new Api(stack, "Api", {
+    customDomain: "rooms.stay-in-athens.com",
     defaults: {
       function: {
         bind: [table],
       },
     },
     routes: {
-      "GET /rooms": "packages/functions/src/rooms.main",
+      "GET /rooms": {
+        function: {
+          handler: "packages/functions/src/rooms.main",
+        },
+      },
+      "GET /hello-world": {
+        function: {
+          handler: "packages/functions/src/hello-world.main",
+        },
+      },
       "POST /rooms": "packages/functions/src/create.main",
     },
   });
+
+  // new Function(stack, "TestFunction", {
+  //   handler: "packages/functions/src/hello-world.main",
+  // });
+
+  //ApiGatewayV1Api
   // Show the API endpoint in the output
   stack.addOutputs({
     ApiEndpoint: api.url,
+    CustomDomainUrl: api.customDomainUrl,
   });
 
   // Return the API resource
